a19037c5bc304baf497a23253fd89dbb40d0197a
Abdurasul <abdurasul15.10.1998@gmail.com>
2026-01-19 13:18:33 +0500

    postgresql

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qd1fghyic8b33pav35=zg$up)qdlzzrl364xr5xaot(n_%i#&h'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -63,12 +68,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # Password validation
@@ -128,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abdurasul15.10.1998@gmail.com'  # <-- SHU JOYGA O'Z EMAILINGIZNI YOZING
-EMAIL_HOST_PASSWORD = 'dyhw eiuj uygw dxyh'  # <-- SHU JOYGA APP PASSWORD NI YOZING (16 ta belgi)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # <-- SHU JOYGA O'Z EMAILINGIZNI YOZING
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # <-- SHU JOYGA APP PASSWORD NI YOZING (16 ta belgi)
 DEFAULT_FROM_EMAIL = 'abdurasul15.10.1998@gmail.com'
 
 EMAIL_VERIFICATION_REQUIRED = True
